4d2e4269e206d0253474bdc36cefa10ed84081a9
anasmohsen51132-star <anasmohsen51132@gmail.com>
2026-05-20 21:39:11 +0300

    Add Express app setup with middleware and routes

---
+++ src/lib/src/utils/src/utils/src/modules/auth/src/modules/auth/src/routes/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import authRoutes from "./src/routes/auth.routes";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+
+app.use("/api/auth", authRoutes);
+
+export default app;
