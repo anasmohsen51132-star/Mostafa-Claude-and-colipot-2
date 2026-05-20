7bf03d84cc90f780d4a529bfa33f68160b6b6a86
anasmohsen51132-star <anasmohsen51132@gmail.com>
2026-05-20 22:02:55 +0300

    Create auth.ts

---
+++ src/config/src/middlewares/src/services/src/services/src/api/src/context/src/services/src/services/src/lib/src/api/src/store/auth.ts
@@ -0,0 +1,22 @@
+import create from "zustand";
+
+interface User {
+  id: string;
+  email: string;
+  role: "OWNER" | "ADMIN" | "STUDENT";
+  name: string;
+}
+
+interface AuthState {
+  user: User | null;
+  accessToken: string | null;
+  setAuth: (user: User, token: string) => void;
+  clearAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  accessToken: null,
+  setAuth: (user, token) => set({ user, accessToken: token }),
+  clearAuth: () => set({ user: null, accessToken: null }),
+}));
